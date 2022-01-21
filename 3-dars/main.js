4ff24c64730d7fc5758d366cb9e1e30d47d5760f
unknown <mengniyozovogabek02@gmail.com>
2022-01-21 22:11:11 +0500

    github search

--- 3-dars/main.js
+++ 3-dars/main.js
@@ -1,3 +1,33 @@
+document.querySelector('form').addEventListener('submit', async function(e) {
+    try {
+        e.preventDefault();
+
+        let username = document.querySelector('input').value;
+        let fetchUrl =  `https://api.github.com/users/${username}`;
+        let repoUrl = `https://api.github.com/users/${username}/repos`
+
+        let response = await fetch(fetchUrl);
+        let repo = await fetch(repoUrl);
+        let repository = await repo.json();
+        
+        repository.forEach(function(repo) {
+            document.querySelector('ul').innerHTML += `<li>${repo.name}</li>`
+        })
+
+        if(!response.ok) {
+            throw new Error('user not found')
+        }
+
+        let user = await response.json();
+        document.querySelector('img').src = user.avatar_url;
+
+       
+       
+    } catch(e) {
+        console.log(e);
+    };
+});
+
 // Github API bilan:
 // - githubdan userlarni search qilish
 // - repo’larni search qilish (malum bir user uchunmas umumiy githubdagi barcha repo’lar orasidan)
