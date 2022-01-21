70434bc16b83424d562c64d0ad8bb600b2bafd07
unknown <mengniyozovogabek02@gmail.com>
2022-01-21 23:43:26 +0500

    github search finish

--- 3-dars/main.js
+++ 3-dars/main.js
@@ -2,29 +2,32 @@ document.querySelector('form').addEventListener('submit', async function(e) {
     try {
         e.preventDefault();
 
+        document.querySelector(".body").style.display = "block"
+
         let username = document.querySelector('input').value;
         let fetchUrl =  `https://api.github.com/users/${username}`;
         let repoUrl = `https://api.github.com/users/${username}/repos`
 
         let response = await fetch(fetchUrl);
         let repo = await fetch(repoUrl);
-        let repository = await repo.json();
-        
-        repository.forEach(function(repo) {
-            document.querySelector('ul').innerHTML += `<li>${repo.name}</li>`
-        })
-
         if(!response.ok) {
             throw new Error('user not found')
         }
 
+        let repository = await repo.json();
+        
+        repository.forEach(function(repo) {
+            document.querySelector('ul.repo-list').innerHTML += `<li>${repo.name}</li>`
+        })   
+
         let user = await response.json();
         document.querySelector('img').src = user.avatar_url;
+        document.querySelector(".username").innerHTML = user.name
 
        
        
     } catch(e) {
-        console.log(e);
+        document.querySelector(".repository").innerHTML = "<h1>User not found</h1>"
     };
 });
 
