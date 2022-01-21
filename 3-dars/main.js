e3801f9527824df05d5e629fcf8f82a41c6ed2e3
unknown <mengniyozovogabek02@gmail.com>
2022-01-21 21:39:01 +0500

    3-dars

---
+++ 3-dars/main.js
@@ -0,0 +1,4 @@
+// Github API bilan:
+// - githubdan userlarni search qilish
+// - repo’larni search qilish (malum bir user uchunmas umumiy githubdagi barcha repo’lar orasidan)
+// - topilgan userni repo’larini sahifaga chiqarib bera olish
